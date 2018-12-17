6a9975a670df2e42c26634bc146d0181c14bd1cf
Kishore Devaraj <kishoregrylls@gmail.com>
2018-12-17 17:52:42 +0530

    Correcting then in if condition

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git checkout -b gh-pages
 npm run build 
 prog=$(basename “$0”)
 for file in $(ls -a);do 
-	if [ "${file}" != "build" -a "${file}" != "." -a "${file}" != ".." -a "${file}" != ".git"  -a “{file}” != “${prog}” ];then
+	if [ "${file}" != "build" -a "${file}" != "." -a "${file}" != ".." -a "${file}" != ".git"  -a “{file}” != “${prog}” ]; then
 		rm -rf ${file}; 
 		echo “File $file deleted.”
 	fi;
