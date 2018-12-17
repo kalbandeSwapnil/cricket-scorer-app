5eb8fbda942a1a180a0a96267116b0e70ce2b62d
Kishore Devaraj <kishoregrylls@gmail.com>
2018-12-17 17:40:12 +0530

    Adding deployment script deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git branch -D gh-pages
+git checkout -b gh-pages
+npm run build 
+prog=$(basename “$0”)
+for file in $(ls -a);do 
+	if [ "${file}" != "build" -a "${file}" != "." -a "${file}" != ".." -a "${file}" != ".git"  - a “{file}” != “${prog}” ];then
+		rm -rf ${file}; 
+		echo “File $file deleted.”
+	fi;
+done;
+git commit -a -m “Adding build @$(date)”
