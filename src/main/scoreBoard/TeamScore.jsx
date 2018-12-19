d92b09f4e19b04cf7ae64ea22ea1f06c5d9faa36
Kishore Devaraj <kishoregrylls@gmail.com>
2018-12-19 11:43:33 +0530

    |#S03|Kishore + Surbhi| added more test cases for routing

--- src/main/scoreBoard/TeamScore.jsx
+++ src/main/scoreBoard/TeamScore.jsx
@@ -5,27 +5,27 @@ import {connect} from 'react-redux';
  export const TeamScore = (props) => {
      let isBold = true;
         return (
-            <div >
-                <div style={MainViewStyle}>
+            <div className="teamName-container" >
+                <div className="team1-container" style={MainViewStyle}>
                     <div className="teamName" style={TeamRunStyle}>
                     <div style ={isBold ? teamBoldStyle : teamNormalStyle}>{props.team1.teamName}</div>
                     </div>
                     <div style={OverViewStyle}>
                     <div className="teamScore" style ={isBold ? scoreBoldStyle : scoreNormalStyle}>{props.team1.runs}/{props.team1.wickets}</div>
-                    <div className="inStatic" style ={isBold ? teamBoldStyle : teamNormalStyle}> &nbsp; in &nbsp;</div>
+                    <div className="inStatic" style ={isBold ? teamBoldStyle : teamNormalStyle}> in </div>
                         <div className="currentBallAndOver" style={isBold ? scoreBoldStyle : scoreNormalStyle}>
                             {props.team1.currentOver + '.'}{props.team1.currentBall + '/'}{props.team1.totalOver}
                         </div>
                     </div>
                 </div>
 
-                <div style={MainViewStyle}>
+                <div className="team2-container" style={MainViewStyle}>
                     <div className="teamName" style={TeamRunStyle}>
                         <div style ={isBold ? teamBoldStyle : teamNormalStyle}>{props.team2.teamName}</div>
                     </div>
                     <div style={OverViewStyle}>
                         <div className="teamScore" style ={isBold ? scoreBoldStyle : scoreNormalStyle}>{props.team2.runs}/{props.team2.wickets}</div>
-                        <div className="inStatic" style ={isBold ? teamBoldStyle : teamNormalStyle}> &nbsp; in &nbsp;</div>
+                        <div className="inStatic" style ={isBold ? teamBoldStyle : teamNormalStyle}> in </div>
                         <div className="currentBallAndOver" style={isBold ? scoreBoldStyle : scoreNormalStyle}>
                             {props.team2.currentOver + '.'}{props.team2.currentBall + '/'}{props.team2.totalOver}
                         </div>
--- src/test/stats/stats.test.js
+++ src/test/stats/stats.test.js
@@ -4,9 +4,9 @@ import Adapter from 'enzyme-adapter-react-16';
 import { MemoryRouter } from 'react-router';
 
 import App from '../../App/'
+import { TeamScore } from '../../main/scoreBoard/TeamScore'
 import CricketGame from '../../main/cricketGame/CricketGame'
 import Stats from '../../main/stats/Stats' 
-import { createCipher } from 'crypto';
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -37,3 +37,79 @@ test('/stats path should redirect to Stats Component', () => {
     expect(wrapper.find(CricketGame)).toHaveLength(1);
     expect(wrapper.find(Stats)).toHaveLength(0);
   });
+
+
+
+  /*
+  *Test cases to check link is there in the components or not
+  */
+
+  test('/ path should contains stats link', () => {
+    const wrapper = mount(
+        <Provider store={testStore}>
+            <MemoryRouter initialEntries={[ '/' ]}>
+                <App/>
+            </MemoryRouter>
+        </Provider>
+    );
+
+    expect(wrapper.exists('.button-stats')).toEqual(true)
+    expect(wrapper.find('.button-stats').text()).toEqual('Stats')
+  });
+
+  test('/stats path should contains home link', () => {
+    const wrapper = mount(
+        <Provider store={testStore}>
+            <MemoryRouter initialEntries={[ '/stats']}>
+                <App/>
+            </MemoryRouter>
+        </Provider>
+    );
+
+    expect(wrapper.exists('.button-stats')).toEqual(true)
+    expect(wrapper.find('.button-stats').text()).toEqual('Home')
+  });
+
+
+
+
+
+/** 
+ * Test cases to check the view part of the rendered component
+ */
+
+let state = {
+    team1: {
+        teamName: 'Team 1',
+        runs: 100,
+        wickets: 6,
+        currentBall: 2,
+        currentOver: 9,
+        totalOver: 20
+
+    },
+    team2: {
+        teamName: 'Team 2',
+        runs: 150,
+        wickets: 8,
+        currentBall: 0,
+        currentOver: 0,
+        totalOver: 20
+    }
+}
+
+
+test('/ path should redirect to CricketGame Component', () => {
+    const wrapper = mount(
+        <Provider store={testStore}>
+            <MemoryRouter initialEntries={[ '/' ]}>
+                <App/>
+            </MemoryRouter>
+        </Provider>
+    )
+
+    const wrapperComponent = mount(<TeamScore team1={state.team1} team2={state.team2} />)
+    expect(wrapperComponent.find('.team1-container').text()).toEqual("Team 1100/6 in 8.2/20")
+    expect(wrapperComponent.find('.team2-container').text()).toEqual("Team 2150/8 in 20")
+})
+
