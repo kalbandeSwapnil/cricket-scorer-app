7ba3dcb34c78353f8693862f7c2b9677405162aa
Kishore Devaraj <kishoregrylls@gmail.com>
2018-12-20 15:45:32 +0530

    |S10| Kishore + Satish| Added utils to generate bowling and batting stats for the players

--- src/main/utils/playerUtils.js
+++ src/main/utils/playerUtils.js
@@ -1,3 +1,4 @@
+// To create a new brand new player
 export const createPlayer = (name, id) => {
     return {
         name: name,
@@ -18,7 +19,57 @@ export const createPlayer = (name, id) => {
     }
 }
 
-export const computeBowlingDetails = (name, overs) => {
+// Function to get the batting and bowling stats of the given player
+export const getPlayerStats = (player, overs) => {
+    const grouped = groupPlayerDetails(overs, ball => ball.bowlerName)
+    return {
+        batting: computeBattingDetails(grouped, player),
+        bowling: computeBowlingDetails(player.name, overs)
+    }
+}
+
+
+
+
+/****
+ * Local methods
+ */
+const groupPlayerDetails = (overs, keyGetter) => {
+    const dummyMap = new Map()
+    overs.forEach(over => {
+        over.forEach(ball => {
+            const key = keyGetter(ball)
+            const collection = dummyMap.get(key)
+            if(!collection) {
+                dummyMap.set(key, [ball])
+            } else {
+                collection.push(ball)
+            }
+        })
+    })
+    return dummyMap
+}
+
+
+
+const computeBattingDetails = (grouped, player) => {
+    return {
+        runs: grouped.get(player.name).reduce((sum, ball) => {
+            return sum + ball.runs
+        }, 0),
+        fours: grouped.get(player.name).filter(ball => {
+            return (ball.runs === 4 && ball.extras.type === '')
+        }).length,
+        sixes: grouped.get(player.name).filter(ball => {
+            return ball.runs === 6 && ball.extras.type === ''
+        }).length,
+        balls: grouped.get(player.name).filter(ball => {
+            return (ball.extras.type === '' || ball.extras.type === 'Lb' || ball.extras.type === 'B')
+        }).length
+    }
+}
+
+const computeBowlingDetails = (name, overs) => {
     return {
         overs: overs.filter((over) => {
             let updatedOver = over.filter(ball => {
@@ -42,17 +93,14 @@ export const computeBowlingDetails = (name, overs) => {
             return overAllSum
         }, 0),
         wickets: overs.reduce((wickets, over) => {
-            
-            // let wicketsInThisOver = over.reduce((sumOfWickets, ball) => {
-            //     if(ball.out) {
-            //         console.log('He took a wicket')
-            //         return sumOfWickets + 1                }
-            // }, 0)
-            // console.log(wicketsInThisOver)
-            // if(wicketsInThisOver > 0) {
-            //     wickets += wicketsInThisOver
-            //     console.log('here')
-            // }
+            let noOfWicketsInThisOver = over.reduce((sumOfWickets, ball) => {
+                if(ball.out) return sumOfWickets + 1
+                return sumOfWickets
+            }, 0)
+            if(noOfWicketsInThisOver > 0) {
+                wickets += noOfWicketsInThisOver
+                console.log('here')
+            }
             return wickets
         }, 0)
     }
@@ -77,7 +125,7 @@ let dummyOvers = [
         extras: { type: ''},
         isExtras : false,
         out: false,
-        runs:  0
+        runs:  6
     },{
         bowlerName: 'Kishore',
         extras: { type: ''},
@@ -86,25 +134,25 @@ let dummyOvers = [
         runs:  0
     },{
         bowlerName: 'Kishore',
-        extras: { type: ''},
+        extras: { type: 'Wd'},
         isExtras : false,
         out: false,
-        runs:  0
+        runs:  6
     },{
         bowlerName: 'Kishore',
-        extras: { type: ''},
+        extras: { type: 'B'},
         isExtras : false,
         out: true,
-        runs:  0
+        runs:  4
     },{
         bowlerName: 'Kishore',
-        extras: { type: ''},
+        extras: { type: 'Lb'},
         isExtras : false,
         out: false,
         runs:  0
     },{
         bowlerName: 'Kishore',
-        extras: { type: ''},
+        extras: { type: 'Nb'},
         isExtras : false,
         out: false,
         runs:  0
@@ -118,6 +166,11 @@ let dummyOvers = [
     },]
 ]
 
-let player = createPlayer('Kishore', 123)
-player.bowling = computeBowlingDetails(player.name, dummyOvers)
-console.log(player)
+// let player = createPlayer('Kishore', 123)
+// player.bowling = computeBowlingDetails(player.name, dummyOvers)
+// const grouped = groupBowlerDetails(dummyOvers, ball => ball.bowlerName)
+// player.batting = computeBattingDetails(grouped, player)
+// const { batting, bowling } = getPlayerStats(player, dummyOvers)
+// player.batting = batting
+// player.bowling = bowling
+// console.log(player)
